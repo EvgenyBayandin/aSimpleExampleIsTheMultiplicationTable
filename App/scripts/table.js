c0497eebafa6c954e8bcf9dc97d2768d7dcdae2b
Evgeny <109107059+EvgenyBayandin@users.noreply.github.com>
2023-06-27 12:56:37 +0300

    Add files
    Add files

---
+++ App/scripts/table.js
@@ -0,0 +1,30 @@
+const getRandomInRange = (min = 1, max = 9) => {
+  return Math.floor(min + Math.random() * (max - min + 1));
+}
+
+const template = document.querySelector('.template');
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const result = document.querySelector('.result');
+
+let firstNumber = getRandomInRange();
+let secondNumber = getRandomInRange();
+
+template.textContent = `${firstNumber} * ${secondNumber}`;
+
+button.addEventListener('click', () => {
+  if (Number(input.value) === firstNumber * secondNumber) {
+    result.textContent = "Верно";
+		result.style.color = "green";
+		
+    firstNumber = getRandomInRange();
+    secondNumber = getRandomInRange();
+    template.textContent = `${firstNumber} * ${secondNumber}`;
+  } else {
+    result.textContent = "Подумайте ещё";
+		result.style.color = "red";
+  }
+	
+  input.value = '';
+  input.focus();
+})
